490e82e783d314b1bc47eeb9a48eb22c8aa4feda
RoshanBandwala <133961578+RoshanBandwala@users.noreply.github.com>
2024-03-10 10:25:15 +0530

    API CALL ADDED

--- AllEvents.js
+++ AllEvents.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import { ChevronLeftIcon, CalendarIcon, LocationMarkerIcon } from '@heroicons/react/solid'; // Importing necessary icons
+import React, { useState, useEffect } from 'react';
+import { ChevronLeftIcon } from '@heroicons/react/solid'; 
+import { CiCalendarDate, CiLocationOn } from "react-icons/ci";
+import { MdOutlineCurrencyRupee } from "react-icons/md";
 
 const categories = [
   { id: 1, name: 'All Events' },
@@ -8,36 +10,48 @@ const categories = [
   { id: 4, name: 'Tech' },
   { id: 5, name: 'Theater' },
   { id: 6, name: 'Festival' },
-  { id: 7, name: 'Sports' }
-];
-
-const eventsData = [
-  { id: 1, name: 'Event 1', category: 'Music', date: '3rd Feb', time: '10:00 AM to 1:00 PM', location: 'Venue 1', price: 'Rs.2000', image: 'https://images.unsplash.com/photo-1540039155733-5bb30b53aa14?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D' },
-  { id: 2, name: 'Event 1', category: 'Music', date: '3rd Feb', time: '10:00 AM to 1:00 PM', location: 'Venue 1', price: 'Rs.2000', image: 'https://images.unsplash.com/photo-1540039155733-5bb30b53aa14?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D' }, { id: 3, name: 'Event 1', category: 'Music', date: '3rd Feb', time: '10:00 AM to 1:00 PM', location: 'Venue 1', price: 'Rs.2000', image: 'https://images.unsplash.com/photo-1540039155733-5bb30b53aa14?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D' }, { id: 4, name: 'Event 1', category: 'Music', date: '3rd Feb', time: '10:00 AM to 1:00 PM', location: 'Venue 1', price: 'Rs.2000', image: 'https://images.unsplash.com/photo-1540039155733-5bb30b53aa14?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D' }, { id: 5, name: 'Event 1', category: 'Music', date: '3rd Feb', time: '10:00 AM to 1:00 PM', location: 'Venue 1', price: 'Rs.2000', image: 'https://images.unsplash.com/photo-1540039155733-5bb30b53aa14?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D' }, { id: 6, name: 'Event 1', category: 'Music', date: '3rd Feb', time: '10:00 AM to 1:00 PM', location: 'Venue 1', price: 'Rs.2000', image: 'https://images.unsplash.com/photo-1540039155733-5bb30b53aa14?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D' }, { id: 7, name: 'Event 1', category: 'Music', date: '3rd Feb', time: '10:00 AM to 1:00 PM', location: 'Venue 1', price: 'Rs.2000', image: 'https://images.unsplash.com/photo-1540039155733-5bb30b53aa14?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D' },
-  // Add more events here
+  { id: 7, name: 'Sports' },
+  { id: 8, name: 'Parties' }
 ];
 
 const AllEvents = () => {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
-  const [clickedButtonId, setClickedButtonId] = useState(null);
+  const [eventsData, setEventsData] = useState([]);
+  const [error, setError] = useState(null);
 
-  const handleButtonClick = (id) => {
-    // handle the click event on the "Book" button.
-    setClickedButtonId(id);
-    // Perform other actions as needed
-    alert('Event booked!');
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let apiUrl = 'https://api.theeventera.live/api/events/active';
+        if (selectedCategory.id !== 1) {
+          apiUrl = `https://api.theeventera.live/api/events/category/${selectedCategory.name}`;
+        }
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error('Failed to fetch events');
+        }
+        const data = await response.json();
+        setEventsData(data); // assuming data is an array of events
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, [selectedCategory]); // re-run effect when selectedCategory changes
+
+  const handleButtonClick = (eventId) => {
+    alert(`Event ${eventId} booked!`);
   };
 
   const handleGoBack = () => {
-    // handle the click event on the "Go back" button.
-    // Implement your go back logic here
     window.history.back();
   };
 
   return (
     <div className="container mx-auto px-6 md:px-12 py-9 bg-white">
       <div className="flex items-center mb-6">
-        <ChevronLeftIcon className="h-8 w-8 mr-2 text-gray-500 cursor-pointer" onClick={handleGoBack} /> {/* Chevron left icon */}
+        <ChevronLeftIcon className="h-8 w-8 mr-2 text-gray-500 cursor-pointer" onClick={handleGoBack} />
         <h1 className="text-3xl md:text-4xl font-semibold">All Events</h1>
       </div>
       <div className="flex flex-wrap justify-center md:justify-between items-center mb-6">
@@ -53,32 +67,44 @@ const AllEvents = () => {
           ))}
         </div>
       </div>
-      
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-6">
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {eventsData.map(event => (
-          <div key={event.id} className="bg-white p-5 rounded-lg shadow-md" style={{ width: '280px' }}>
-            <img src={event.image} alt={event.name} className="w-full h-60 rounded-lg mb-4" />
-            <h3 className="text-lg md:text-xl font-semibold mb-2">{event.name}</h3>
-            <div className="flex items-center text-gray-600 mb-1">
-              <CalendarIcon className="h-5 w-5 mr-1" />
-              <p>{event.date} | {event.time}</p>
+          <div key={event._id} className="p-1.5 bg-white/100 font-poppins rounded-lg  drop-shadow-xl  transition ">
+            <div className="w-80 h-[308px] rounded-sm overflow-hidden">
+              <img src={`https://api.theeventera.live/${event.poster_img}`} alt="" className="w-full h-full object-cover" />
             </div>
-            <div className="flex items-center text-gray-600 mb-1">
-              <LocationMarkerIcon className="h-5 w-5 mr-1" />
-              <p>{event.location}</p>
-            </div>
-            <div className="flex items-center text-gray-600 font-semibold mb-1">
-              <p>{event.price}</p>
-              <button
-                className={`px-4 py-2 rounded-full ml-auto ${clickedButtonId === event.id ? 'bg-red-500 text-white' : 'bg-white border border-red-500 text-red-500'}`}
-                onClick={() => handleButtonClick(event.id)}
-              >
-                Book
-              </button>
+            <div className="p-2">
+              <h2 className="text-2xl font-poppins text-gray-800 font-semibold">
+                {event.event_name}
+              </h2>
+              <ul className="text-base mt-5 text-gray-700">
+                <li className="flex gap-1 items-center">
+                  <CiCalendarDate size={20} />{" "}
+                  <span>{new Date(event.event_date).toLocaleDateString()}</span> |{" "}
+                  <span>{event.event_time}</span>
+                </li>
+                <li className="flex gap-1 items-center mt-1">
+                  <CiLocationOn size={20} /> <span>{event.city}</span>
+                </li>
+              </ul>
+              <div className="flex items-center mt-3  justify-between mb-5 mx-3">
+                <h3 className="text-lg text-gray-800 flex items-center font-poppins font-medium">
+                  <MdOutlineCurrencyRupee /> {event.price}
+                </h3>
+                <button
+                  className="py-3 px-12 text-base font-light text-red-500 hover:bg-red-500 border-[1px] transition border-red-500 hover:text-white rounded-3xl"
+                  onClick={() => handleButtonClick(event._id)}
+                >
+                  Book
+                </button>
+              </div>
             </div>
           </div>
         ))}
+        
       </div>
+    
     </div>
   );
 };
